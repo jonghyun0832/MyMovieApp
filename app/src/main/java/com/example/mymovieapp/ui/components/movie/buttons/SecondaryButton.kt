a96ddfed75053d1e950e8ff33a60588c52b795d4
서종현 <seojonghyeon@seojonghyeon-ui-MacBookPro.local>
2025-06-28 16:48:58 +0900

    Add Movie Buttons

---
+++ app/src/main/java/com/example/mymovieapp/ui/components/movie/buttons/SecondaryButton.kt
@@ -0,0 +1,66 @@
+package com.example.mymovieapp.ui.components.movie.buttons
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mymovieapp.ui.theme.MyMovieAppTheme
+import com.example.mymovieapp.ui.theme.Paddings
+import com.example.mymovieapp.ui.theme.dialogButton
+
+@Composable
+fun SecondaryButton(
+    modifier: Modifier = Modifier,
+    @StringRes id: Int? = null,
+    text: String = "",
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = modifier.fillMaxWidth(),
+        shape = MaterialTheme.shapes.large,
+        onClick = onClick,
+        border = BorderStroke(
+            2.dp,
+            MaterialTheme.colorScheme.secondary
+        ),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.background,
+            contentColor = MaterialTheme.colorScheme.secondary,
+            disabledContentColor = MaterialTheme.colorScheme.background,
+            disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer
+        )
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = id?.let { stringResource(id) } ?: text,
+                style = MaterialTheme.typography.labelLarge,
+                modifier = Modifier.padding(Paddings.small)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SecondaryButtonPreview() {
+    MyMovieAppTheme {
+        SecondaryButton(
+            text = "CANCEL"
+        ) {}
+    }
+}
---
+++ app/src/main/java/com/example/mymovieapp/ui/components/movie/buttons/UnderlinedTextButton.kt
@@ -0,0 +1,62 @@
+package com.example.mymovieapp.ui.components.movie.buttons
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mymovieapp.ui.theme.MyMovieAppTheme
+import com.example.mymovieapp.ui.theme.Paddings
+import com.example.mymovieapp.ui.theme.dialogButton
+import com.example.mymovieapp.ui.theme.underlinedButton
+import com.example.mymovieapp.ui.theme.underlinedDialogButton
+
+@Composable
+fun UnderlinedButton(
+    modifier: Modifier = Modifier,
+    @StringRes id: Int? = null,
+    text: String = "",
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = modifier.fillMaxWidth(),
+        shape = MaterialTheme.shapes.large,
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.background,
+            contentColor = MaterialTheme.colorScheme.secondary,
+            disabledContentColor = MaterialTheme.colorScheme.background,
+            disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer
+        )
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = id?.let { stringResource(id) } ?: text,
+                style = MaterialTheme.typography.underlinedDialogButton,
+                modifier = Modifier.padding(Paddings.small)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun UnderlinedButtonPreview() {
+    MyMovieAppTheme {
+        UnderlinedButton(
+            text = "SUBMIT"
+        ) {}
+    }
+}
