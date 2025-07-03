bced78d0ef7886cad0077074089b640f776ea51b
JongHyunSeo <jonghyun9606@gmail.com>
2025-07-03 13:17:31 +0900

    clean code

--- app/src/main/java/com/example/mymovieapp/features/dialogs/IMDBDialogScreen.kt
+++ app/src/main/java/com/example/mymovieapp/features/dialogs/IMDBDialogScreen.kt
@@ -1,16 +1,11 @@
 package com.example.mymovieapp.features.dialogs
 
-import android.content.Intent
-import android.net.Uri
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.mymovieapp.R
 import com.example.mymovieapp.ui.components.dialog.Default
 import com.example.mymovieapp.ui.components.dialog.DialogPopup
 import com.example.mymovieapp.ui.models.dialog.DialogButton
-import com.example.mymovieapp.ui.theme.MyMovieAppTheme
 
 @Composable
 fun IMDBDialogScreen(
--- app/src/main/java/com/example/mymovieapp/features/dialogs/RatingDialogFragment.kt
+++ app/src/main/java/com/example/mymovieapp/features/dialogs/RatingDialogFragment.kt
@@ -1,23 +1,12 @@
 package com.example.mymovieapp.features.dialogs
 
-import android.graphics.drawable.ColorDrawable
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.window.Dialog
-import androidx.navigation.fragment.navArgs
 import com.example.mymovieapp.R
 import com.example.mymovieapp.ui.components.dialog.DialogPopup
 import com.example.mymovieapp.ui.components.dialog.Rating
 import com.example.mymovieapp.ui.models.buttons.LeadingIconData
 import com.example.mymovieapp.ui.models.dialog.DialogButton
-import dagger.hilt.android.AndroidEntryPoint
 
 @Composable
 fun RatingDialogScreen(
@@ -50,49 +39,3 @@ fun RatingDialogScreen(
         )
     }
 }
-
-//@AndroidEntryPoint
-//class RatingDialogFragment : BaseDialogFragment() {
-//
-//    private val args: RatingDialogFragmentArgs by navArgs()
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        dialog?.apply {
-//            isCancelable = true
-//            setCanceledOnTouchOutside(true)
-//            window?.setBackgroundDrawable(ColorDrawable(requireContext().getColor(android.R.color.transparent)))
-//        }
-//
-//        return ComposeView(requireContext()).apply {
-//            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-//            setContent {
-//                MovieAppTheme(
-//                    themeState = themeViewModel.themeState.collectAsState()
-//                ) {
-//                    DialogPopup.Rating(
-//                        movieName = args.movieName,
-//                        rating = args.rating,
-//                        buttons = listOf(
-//                            DialogButton.Primary(
-//                                title = getString(R.string.submit),
-//                                leadingIconData = LeadingIconData(
-//                                    iconDrawable = R.drawable.ic_send,
-//                                    iconContentDescription = R.string.submit
-//                                )
-//                            ) {
-//                                dismiss()
-//                            },
-//                            DialogButton.Secondary(getString(R.string.cancel)) {
-//                                dismiss()
-//                            }
-//                        )
-//                    )
-//                }
-//            }
-//        }
-//    }
-//}
